7ac8bd3632b36debb927eb9d000d225e93217ef3
FabricioRR13 <fabricio.r@aluno.senai.br>
2025-03-19 17:54:11 -0300

    aula javaScript

---
+++ scripts/script.js
@@ -0,0 +1,76 @@
+console.log("Hello word");
+
+var nome = "Fabricio";
+let idade = "38";
+const profissao = "Engenheiro";
+
+console.log("ola, meu é "+ nome + ",tenho " +  idade + " anos.")
+let mensagem = "Olá, meu nome é " + nome;
+console.log(mensagem);
+
+mensagem = `Olá meu nome é ${nome}, tenho ${idade} anos e sou ${profissao}`
+console.log(mensagem);
+
+
+// console.log(nome);
+// console.log(idade);
+// console.log(profissao);
+
+nome  = "flavio";
+idade = "30";
+//profissao = "pscologo";
+
+// console.log(nome);
+// console.log(idade);
+// console.log(profissao);
+
+// console.log(typeof nome);
+// console.log(typeof idade);
+
+// let maiorDeIdade = true;
+// let temFilhos = false;
+
+// console.log(typeof maiorDeIdade);
+// console.log(typeof temFilhos);
+
+// let tamanho;
+// console.log(typeof tamanho);
+
+// let vazia = null;
+// console.log(typeof vazia);
+
+let a = 10;
+let b = "2";
+let c = "50";
+let d = 50;
+
+
+
+// console.log(a < d);  //Verifica que menor
+// console.log(a>d);  //ver. se maior
+// console.log(c===d); // ver. tipo e valor
+// console.log(c==d); //ver. somente valor
+// console.log(a !== d); // ver. diferença tipo e valor
+// console.log(c != d); //ver. diferenca somente do valor
+
+
+
+
+
+
+// console.log(a+b);
+// console.log(c+a);
+// console.log(a+d);
+// console.log(a-b);
+
+// console.log(typeof b);
+
+// b = Number (b);
+// console.log(typeof b);
+// console.log(a + b);
+
+// console.log(typeof d);
+// d = toString(d);
+// console.log(typeof d);
+
+
